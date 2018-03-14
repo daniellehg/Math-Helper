5d29156e98dd40138bd8f03c4d5650115585de31
Danielle H Gamble <dg12181@srcs.loc>
2018-03-14 12:21:41 -0700

    test

--- mathApp.js
+++ mathApp.js
@@ -1,4 +1,7 @@
-function Quadratic()
+function Quadratic(){
 var A = document.getElementbyId('a');
 var B = document.getElementById('b');
 var C = document.getElementById('c');
+var x = document.getElementById('solutions');
+x = -b + || - math.sqrt(math.pow2b- 4ac)/2a;
+}
