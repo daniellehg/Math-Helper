dd262a8c0395b8c98056a5986fccd3c07fb1bf39
Danielle H Gamble <dg12181@srcs.loc>
2018-03-23 11:53:44 -0700

    adjustments to the code- still doesnt print;/

--- mathApp.js
+++ mathApp.js
@@ -1,15 +1,15 @@
-var button = document.getElementById('solve')
+var button = document.getElementById('solve');
 
 function Quadratic() {
-    var A = document.getElementbyId('a');
-    var B = document.getElementById('b');
-    var C = document.getElementById('c');
-    var x1 = ((0 - B) + Math.sqrt((B * B) - (4 * A * C))) / 2 * A;
-    var x2 = ((0 - B) - Math.sqrt((B * B) - (4 * A * C))) / 2 * A;
-    if (isNaN(A || B || C) = true) {
-        document.getElementById('solve').textContent = "This is not a number."
+    var A = document.getElementbyId('a').value;
+    var B = document.getElementById('b').value;
+    var C = document.getElementById('c').value;
+    var x1 = ((-B) + Math.sqrt((B * B) - (4 * A * C))) / 2 * A;
+    var x2 = ((-B) - Math.sqrt((B * B) - (4 * A * C))) / 2 * A;
+    if (isNaN(A) || isNaN(B) || isNaN(C) == true) {
+        document.getElementById('solve').textContent = "You entered an incorrect value for either A, B, or C. Please try again."
     } else {
-        document.getElementById('solutions').textContent = x1, "&", x2;
+        document.getElementById('solutions').textContent = "The roots to the quadratic are" x1 "&" x2;
     }
 }
 button.addEventListener('click', Quadratic);
