3e119f1efeea26fedef9df3c287060e779d2c584
Danielle H Gamble <dg12181@srcs.loc>
2018-03-23 11:19:53 -0700

    revision 1 - main code done but doesnt return

--- mathApp.js
+++ mathApp.js
@@ -1,7 +1,15 @@
-function Quadratic(){
-var A = document.getElementbyId('a');
-var B = document.getElementById('b');
-var C = document.getElementById('c');
-var x = document.getElementById('solutions');
-x = -b + || - math.sqrt(math.pow2b- 4ac)/2a;
+var button = document.getElementById('solve')
+
+function Quadratic() {
+    var A = document.getElementbyId('a');
+    var B = document.getElementById('b');
+    var C = document.getElementById('c');
+    var x1 = ((0 - B) + Math.sqrt((B * B) - (4 * A * C))) / 2 * A;
+    var x2 = ((0 - B) - Math.sqrt((B * B) - (4 * A * C))) / 2 * A;
+    if (isNaN(A || B || C) = true) {
+        document.getElementById('solve').textContent = "This is not a number."
+    } else {
+        document.getElementById('solutions').textContent = x1, "&", x2;
+    }
 }
+button.addEventListener('click', Quadratic);
